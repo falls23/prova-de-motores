6c3f27d2d39335bd507528f96ffcb72a2e717f24
falls23 <g.goulart@escolar.ifrn.edu.br>
2025-11-16 19:45:16 -0300

    16/11/25

--- Assets/script/InimigoVoador.cs
+++ Assets/script/InimigoVoador.cs
@@ -84,7 +84,7 @@ public class InimigoVoador : MonoBehaviour
                    player.transform.position, 
                    velocidade * Time.deltaTime);
               
-               spriteRenderer.flipX = false;
+               spriteRenderer.flipX = true;
             }
             else
             //esquerda
@@ -95,7 +95,7 @@ public class InimigoVoador : MonoBehaviour
                     player.transform.position, 
                     velocidade * Time.deltaTime);
                 
-                spriteRenderer.flipX = true;
+                spriteRenderer.flipX = false;
             }
          
        
